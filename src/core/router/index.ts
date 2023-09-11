caa282a931ff51e25a04e1e1a938d33049835074
imkhoat <khoatbui.idea@gmail.com>
2023-09-11 09:11:53 +0700

    feat: add home page and link to note module

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { noteRoutes, NOTE_ROUTE_NAMES } from '@/modules/notes/router';
+import { homeRoutes, HOME_ROUTE_NAMES } from '@/modules/home/router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...noteRoutes],
+  routes: [...noteRoutes, ...homeRoutes],
 });
 
 export const ROUTE_NAMES = {
   NOTE_ROUTE_NAMES,
+  HOME_ROUTE_NAMES,
 };
 export default router;
