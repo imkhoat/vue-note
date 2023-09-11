5fbfb7e0899f914c92fe41472d5296384697e991
imkhoat <khoatbui.idea@gmail.com>
2023-09-11 21:36:10 +0700

    feat: support press Ctr key to create new note

--- src/modules/notes/_partials/empty-card.vue
+++ src/modules/notes/_partials/empty-card.vue
@@ -3,12 +3,15 @@
     class="flex flex-col justify-center items-center gap-y-4 w-full h-48 m-auto bg-gray-50 border-dashed border-2 border-gray-100 rounded-xl text-gray-300"
   >
     <i-archive class="!w-16 !h-16"></i-archive>
-    <p>
-      No notes found, click
+    <p class="text-center">No notes found</p>
+    <p class="text-center">
+      Click
       <b-button size="xs" rounded square>
-        <i-plus class="w-2 h-2"></i-plus>
+        <i-plus class="w-[16px] h-[16px]"></i-plus>
       </b-button>
-      to create new one!
+      or press
+      <b-button size="xs" square variant="secondary" class="text-sm">Ctr/Control</b-button> key to
+      create new one!
     </p>
   </div>
 </template>
